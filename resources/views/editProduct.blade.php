bedbf6def86a078dbfdfede19caac890777654b8
kaishui7193 <goh7193@gmail.com>
2021-06-19 15:21:49 +0800

    testing

--- resources/views/editProduct.blade.php
+++ resources/views/editProduct.blade.php
@@ -7,7 +7,7 @@
         </div>       
 @endif 
 
-
+<!-- fdfsadfsafa -->
 <link rel="preconnect" href="https://fonts.gstatic.com">  
 <link href="https://fonts.googleapis.com/css2?family=Teko:wght@500&family=Catamaran:wght@500&display=swap" rel="stylesheet">
 
