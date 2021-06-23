a6a4bdc8d6c00c62158d78b9991e7df5ed8fafc3
NG SHU LING <D190038A@sc.edu.my>
2021-06-23 14:17:24 +0800

    product page

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,40 +1,66 @@
 @extends('layouts.app')
-@section('content') 
-@if(Session::has('success'))           
-        <div class="alert alert-success" role="alert">
-            {{ Session::get('success')}}
-        </div>       
-@endif 
-    
-<div id="templatemo_main">
+@section('content')
+
+@if(Session::has('success'))
+<div class="alert alert-success" role="alert">
+    {{Session::get('success')}}
+
+</div>
+@endif
+@if(Session::has('paymentSuccess'))
+<div class="alert alert-success" role="alert">
+
+    {{Session::get('paymentSuccess')}}
+
+</div>
+@endif
+
+<body>
+<link href="css/product.css" rel="stylesheet" type="text/css" />
+
+<div id="space">
 
 
 <div id="sidebar">
-    <h3>Categories</h3>
-
-    <ul class="sidebar_menu">
-        <li><a href="{{route('products')}}">All</a></li>
+  
+<table>
+  
+       <th id="th"> <a href="{{route('products')}}">All</a></th>
 
         @foreach($categories as $category)
-        <li><a href="{{route('products',['category'=>$category->id])}}">{{$category->name}}</a></li>
-
+    <th>    <a href="{{route('products',['category'=>$category->id])}}">{{$category->name}}</a>
+</th>
         @endforeach
-    </ul>
+</table>
 
 </div> <!-- END of sidebar -->
 
+<hr color="grey" ></hr>
 
+<br>
 
+@if($categoryName==null)
+                <h2>Product</h2>
+                @else
+                @foreach ($categoryName as $CName)
+
+                <h2>{{$CName->name}}</h2>
+                @endforeach
+                @endif
+
+                <br>
 <div class="col-md-12">
             <div class="card border-0">
-                <h5 class="card-title">Products</h5>                   
+                             
                  <div class="row">
                 @foreach($products as $product)              
                     <div class="col-sm-4">
                         <div class="card h-100">
                             <div class="card-body">
-                                    <h5 class="card-title">{{$product->bookName}}</h5>                            
-                                     <a href="{{ route('product.detail', ['id' => $product->id]) }}"><img src="{{ asset('images/') }}/{{$product->image}}" alt="" class="img-fluid"></a>
+                                    <h5 class="card-title" >{{$product->bookName}}</h5>  
+                                    <br>                          
+                                     <a href="{{ route('product.detail', ['id' => $product->id]) }}"><img src="{{ asset('images/') }}/{{$product->image}}" alt="" class="img-fluid" width="250px" ></a>
+                                     <br>
                                      <div class="card-heading">RM {{$product->price}} <button style="float:right;" class="btn btn-danger btn-xs">AddToCart</button>
                                     </div>
                             </div>
