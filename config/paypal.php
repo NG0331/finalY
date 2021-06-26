80d0632debd113e9d46c85827865487bcb61bebd
NG SHU LING <D190038A@sc.edu.my>
2021-06-26 20:04:27 +0800

    paypal done

--- config/paypal.php
+++ config/paypal.php
@@ -1,12 +1,12 @@
 <?php 
 return [ 
-    'client_id' => 'Enter Your Client ID',
-	'secret' => 'Enter Your Secret Key',
+    'client_id' => env('PAYPAL_CLIENT_ID',''),
+    'secret' => env('PAYPAL_SECRET',''),
     'settings' => array(
-        'mode' => 'sandbox',
-        'http.ConnectionTimeOut' => 1000,
+        'mode' => env('PAYPAL_MODE','sandbox'),
+        'http.ConnectionTimeOut' => 30,
         'log.LogEnabled' => true,
         'log.FileName' => storage_path() . '/logs/paypal.log',
-        'log.LogLevel' => 'FINE'
+        'log.LogLevel' => 'ERROR'
     ),
 ];
