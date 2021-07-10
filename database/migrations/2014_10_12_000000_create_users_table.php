e1fbf3b194d43539bdf3ad9316e2227623f3ea19
kaishui7193 <goh7193@gmail.com>
2021-07-10 20:49:58 +0800

    7/10_address
    loginAddressDone_7/10

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->boolean('is_admin')->nullable();
+            $table->string('address');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
